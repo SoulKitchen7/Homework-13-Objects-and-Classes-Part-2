985d40462a5c27ef56f0ca95d58c319a8a04ae57
Пользователь <whiskygogo@mail.ru>
2023-02-01 17:37:46 +0300

    Homework - 13 done.

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,50 @@
+import java.util.Objects;
+
 public class Book {
+    private String titleOfTheBook;
+    public int yearOfPublication;
+    public Author author;
+
+    public Book(String titleOfTheBook, Author author, int yearOfPublication) {
+        this.titleOfTheBook = titleOfTheBook;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getTitleOfTheBook() {
+        return this.titleOfTheBook;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && titleOfTheBook.equals(book.titleOfTheBook) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titleOfTheBook, yearOfPublication, author);
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Название книги - " + titleOfTheBook + ", год издания - " + yearOfPublication + ", Автор - " + author;
+
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author erichRemarque = new Author("Эрих", "Ремарк");
+        Book threeLoads = new Book("Три товарища", erichRemarque, 1936);
+        System.out.println(threeLoads);
+
+        System.out.println("");
+
+        Author mikhailBulgakov = new Author("Михаил", "Булгаков");
+        Book theMasterAndMargarita = new Book("Мастер и Маргарита", mikhailBulgakov, 1967);
+
+        System.out.println(theMasterAndMargarita);
+
     }
 }
